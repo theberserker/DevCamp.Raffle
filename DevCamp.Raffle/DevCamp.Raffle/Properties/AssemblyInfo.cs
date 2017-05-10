451a88c0e1a3cd0d1a18f0f38b8da57e423b5219
Andrej Spilak <aspilak@pixi.eu>
2017-05-10 18:40:12 +0200

    titles altered

--- DevCamp.Raffle/DevCamp.Raffle/Properties/AssemblyInfo.cs
+++ DevCamp.Raffle/DevCamp.Raffle/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DevCamp.Raffle")]
+[assembly: AssemblyTitle("pixi* DevCamp Raffle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DevCamp.Raffle")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("pixi* DevCamp Raffle")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
