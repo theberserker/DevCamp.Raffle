0bdd94fb9c1d861911b9d5e04b18f47633d8cd11
Andrej Spilak <aspilak@pixi.eu>
2017-05-11 08:44:23 +0200

    Presentation startpoint

--- DevCamp.Raffle/DevCamp.Raffle.Android/MainActivity.cs
+++ DevCamp.Raffle/DevCamp.Raffle.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DevCamp.Raffle.Droid
 {
-    [Activity(Label = "pixi* DevCamp Raffle", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "pixi* DevCamp Raffle", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
